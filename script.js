9327d67626579cba3acc7899ac7482cd78e91b87
bdmanik97 <inmanik97@gmail.com>
2024-01-17 22:17:16 +0600

    {Javascriptfinal}

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// twoDimentionalArray for loop start here
+// let manik = {name:"manik", age:'28', blance:'2025452', bank: 'DBBL', id: '1012235A', area: 'DHK-3', phone: '01722310503'};
+//     // console.log(manik.phone);
+// for(let key in manik){
+//     console.log(manik[key])
+// }
+
+// twoDimentionalArray for loop end here
+
+// multi diamensional array
+let twoDimensionalArray = [
+    ['Rohim', 'Korim', 'Bablu', 'Manik', 'Ratul'],
+    ['Biploby', 'Susmita', 'Moni', 'Mily', 'Nasir',],
+    ['Manikul', 'Nasir', 'Mamun', 'Prince', 'Avijit'],
+    ['Dony', 'Kamona', 'Bonni', 'Kanmona', 'Bhumika'],
+]
+
+for(let i=0; i<=3; i++){
+
+    for(let j=0; j<=4; j++){
+        console.log(twoDimensionalArray[i][j]);
+    }
+}
+
+// Associative array
